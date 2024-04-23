6f6cb5a889391f33ee88bce3f813ddd43315fe41
sharmaharsh1991 <harshodesk@gmail.com>
2024-04-23 14:00:00 +0100

    Add some chnages

--- backend/db/dataBase.js
+++ backend/db/dataBase.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host:process.env.HOST,
 	user:process.env.DB_USER, //Your Database User Name
-	password:process.env.DB_PASSWORD, // Your Database Password
+	password:process.env.DB_PASSWORD, // Your Database Password  
     database: process.env.DATABASE,
     insecureAuth : true
 });
